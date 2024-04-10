206d6715a6bc3ecb0c37582d99d516c9cb7e1ee3
Holden Chih <catcherDB@hotmail.com>
2024-04-10 15:03:55 +0800

    jklhl678

--- JavaTypeVariable.java
+++ JavaTypeVariable.java
@@ -3,15 +3,38 @@ public class JavaTypeVariable{
         // System.out.println("Hello Java!!");
         // System.out.println("Hello Java!! second");
 
-        // 整數
-        System.out.println(3);
-        System.out.println(120);
+        // 整數，預設是 int 型態
+        int x = 3;
+        System.out.println(x);
+        x = 120;
+        System.out.println(x);
 
         // 長整數，數字的後面特別加上 L
-        System.out.println(200000000000L);
-
+        long y = 200000000000L;
+        System.out.println(y);
+ 
         // 浮點數，預設為 double 型態
-        System.out.println(3.14159268);
+        double m = 3.14159268;
+        System.out.println(m);
+
+        // 單精度浮點數，數字後面特別加上 F
+        float n = 3.14159268F;
+        System.out.println(n);
+
+        // 布林值
+        boolean b;
+        b = true;
+        System.out.println(b);
+        
+        // 字元
+        char c = '我';
+        System.out.println(c);
+
+        // 字串
+        String s = "哈囉你好嗎?";
+        System.out.println(s);
+        s = "heLLo Java!!";
+        System.out.println(s);
     }
 
 }
