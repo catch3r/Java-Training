c45194055378e05d4e911ecfb1c70923f98700b1
Holden Chih <catcherDB@hotmail.com>
2024-04-10 14:45:07 +0800

    first commit

---
+++ JavaTypeVariable.java
@@ -0,0 +1,17 @@
+public class JavaTypeVariable{
+    public static void main(String[] args){
+        // System.out.println("Hello Java!!");
+        // System.out.println("Hello Java!! second");
+
+        // 整數
+        System.out.println(3);
+        System.out.println(120);
+
+        // 長整數，數字的後面特別加上 L
+        System.out.println(200000000000L);
+
+        // 浮點數，預設為 double 型態
+        System.out.println(3.14159268);
+    }
+
+}
